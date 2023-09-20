06b6abac3233f7958971999a1313baf59a5a858d
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-09-20 10:50:36 +0600

    React Loader sniper added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Phones from "../Phones/Phones";
 import ReCharts from "../ReCharts/ReCharts";
+import { RotatingLines } from "react-loader-spinner";
+
+
 
 const Home = () => {
+    const navigation = useNavigation();
+    // console.log(navigation);
     return (
         <>
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                navigation.state === "loading" ?
+                    <div className="flex justify-center">
+                        <RotatingLines>
+                            strokeColor="grey"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="96"
+                            visible={true}
+                        </RotatingLines>
+                    </div>
+                    :
+                    <Outlet></Outlet>
+            }
             {/* <Phones></Phones> */}
         </>
     );
